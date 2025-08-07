3e3a55085419c8d4920c8f712c53ea7b0d657892
Zoltan Karahuta <zoltan.karahuta@indisemi.com>
2025-08-07 15:41:44 +0200

    update

--- sw/linux/hps_lcd/main.c
+++ sw/linux/hps_lcd/main.c
@@ -40,17 +40,13 @@
 #define HW_REGS_MASK ( HW_REGS_SPAN - 1 )
 
 void flip_content_vert(LCD_CANVAS *canvas) {
-    uint8_t *pFrame_new;
     uint8_t *pFrame_old = canvas->pFrame;
-    int i;
-    int j;
+    //uint8_t *pFrame_new = (void *)malloc(canvas->FrameSize);
+    uint8_t *pFrame_new = (uint8_t *)malloc(canvas->FrameSize);
+    int i, j;
     
-    pFrame_new = (void *)malloc(canvas->FrameSize);
     for (i = 0; i < canvas->FrameSize; i = i+1 ) {
-        //pFrame_new = pFrame_new + i;
-        //*(pFrame_new + ((canvas->FrameSize)-i)) =  *(canvas->pFrame)
-        //*pFrame_new = *(canvas->pFrame);
-        pFrame_new[(canvas->FrameSize)-i-1] = (canvas->pFrame)[i];
+        pFrame_new[(canvas->FrameSize)-i-1] = (pFrame_old)[i];
     }
     canvas->pFrame = pFrame_new;  
     free(pFrame_old); /* Free old frame, replaced by new one */
